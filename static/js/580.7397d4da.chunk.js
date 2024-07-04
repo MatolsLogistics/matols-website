24bda79ab481ff0922afae14a9dfd4fa154b5df9
Freddy Smith <sdumosmith@gmail.com>
2024-07-04 13:47:02 +0200

    version 1.1

---
+++ static/js/580.7397d4da.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmatols_website=self.webpackChunkmatols_website||[]).push([[580],{9580:(e,s,t)=>{function u(){}t.r(s),t.d(s,{default:()=>u})}}]);
+//# sourceMappingURL=580.7397d4da.chunk.js.map
